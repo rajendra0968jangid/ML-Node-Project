915e818f3b875aafcfb1b96a2faad5647b58d2b7
rajendra0968jangid <rajendra0968jangid@gmail.com>
2026-04-29 15:30:55 +0530

    code

--- python-api/app.py
+++ python-api/app.py
@@ -4,7 +4,7 @@ import pickle
 app = Flask(__name__)
 
 # Load model
-with open("model.pkl", "rb") as file:
+with open("../model.pkl", "rb") as file:
     model = pickle.load(file)
 
 @app.route("/predict", methods=["POST"])
@@ -14,13 +14,14 @@ def predict():
     input_data = [[
         data["Attendance"],
         data["Test_Marks"],
-        data["Assignment_Marks"]
+        data["Assignment_Marks"],
+        data["Practical_Marks"]
     ]]
 
     prediction = model.predict(input_data)
 
     return jsonify({
-        "predicted_marks": float(prediction[0])
+        "predicted_marks": float(prediction[0][0])
     })
 
 if __name__ == "__main__":
